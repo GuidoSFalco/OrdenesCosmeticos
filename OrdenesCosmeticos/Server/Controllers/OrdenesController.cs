ef6d3a4cadbdd1733be9ec79a9daa90eae88620f
GuidoSFalco <66317039+GuidoSFalco@users.noreply.github.com>
2021-10-29 17:41:55 -0300

    ordenes get y post

---
+++ OrdenesCosmeticos/Server/Controllers/OrdenesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrdenesCosmeticos.Shared.Data;
+using OrdenesCosmeticos.Shared.Data.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdenesCosmeticos.Server.Controllers
+{
+    [ApiController]
+    [Route("api/ordenes")]
+    public class OrdenesController : ControllerBase
+    {
+        private readonly dbContext context;
+
+        public OrdenesController(dbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Orden>> Get()
+        {
+            //return await context.Paises.Include(x => x.Provincias).ToListAsync();
+            return context.Ordenes.ToList();
+        }
+
+        [HttpPost]
+        public ActionResult<Orden> Post(Orden orden)
+        {
+            
+                context.Ordenes.Add(orden);
+                context.SaveChanges();
+                return orden;
+            
+        }
+    }
+}
